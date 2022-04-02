0f00d2008c5b8006de6265133a0cba7c8fe8b07b
Milan Vaško <m.vasko2@gmail.com>
2022-04-02 23:08:37 +0200

    Generate random trash all over the place

--- random_trash/random_trash.gd
+++ random_trash/random_trash.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 export(Array) var textures: Array
+export(Array) var colors: Array
 
 func _ready():
 	var sprite: Sprite = $Sprite
 	sprite.texture = textures[randi() % textures.size()]
 	sprite.rotation = randf() * deg2rad(360.0)
 	sprite.scale = Vector2.ONE * max(randf(), 0.7)
+	sprite.modulate = colors[randi() % colors.size()]
---
+++ random_trash/random_trash_bulk.gd
@@ -0,0 +1,30 @@
+tool
+extends Node2D
+
+export(Rect2) var rect: Rect2
+export(int) var trash_count: int
+
+func _ready() -> void:
+	if !Engine.editor_hint:
+		generate_trash()
+
+func generate_trash() -> void:
+	for i in range(trash_count):
+		spawn_trash_at_position(Vector2(
+			rect.position.x + (randf() * rect.end.x),
+			rect.position.y + (randf() * rect.end.y)
+		))
+
+func spawn_trash_at_position(pos: Vector2) -> void:
+	var random_trash_scene := preload("res://random_trash/random_trash.tscn")
+	var random_trash = random_trash_scene.instance()
+	add_child(random_trash)
+	random_trash.position = pos
+
+func _draw() -> void:
+	if Engine.editor_hint:
+		draw_rect(rect, Color.red, false, 2.0)
+
+func _process(_delta: float) -> void:
+	if Engine.editor_hint:
+		update()
