bab84aac18fc4840fb92454768cece297a9890ee
Milan Vaško <m.vasko2@gmail.com>
2022-04-02 13:48:43 +0200

    Add stamina

--- ingame_gui/ingame_gui.gd
+++ ingame_gui/ingame_gui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var health_bar := $Stats/Health/ProgressBar
+onready var stamina_bar := $Stats/Stamina/ProgressBar
 onready var hunger_bar := $Stats/Hunger/ProgressBar
 onready var coldness_bar := $Stats/Coldness/ProgressBar
 onready var death_screen := $DeathScreen
@@ -10,6 +11,9 @@ func _ready():
 
 func _on_health_changed(new_health: float) -> void:
 	health_bar.value = new_health * health_bar.max_value
+
+func _on_stamina_changed(new_stamina: float) -> void:
+	stamina_bar.value = new_stamina * stamina_bar.max_value
 
 func _on_hunger_changed(new_hunger: float) -> void:
 	hunger_bar.value = new_hunger * hunger_bar.max_value
--- player/player.gd
+++ player/player.gd
@@ -7,16 +7,22 @@ const COLDNESS_HEALTH_DECAY_PER_SECOND := 0.001
 const HUNGER_FILL_PER_SECOND := 0.005
 const HUNGER_HEALTH_DECAY_PER_SECOND := 0.001
 const FIRE_DECAY_PER_SECOND := 0.2
+const STAMINA_REPLENISH_PER_SECOND := 0.1
+const STAMINA_DECAY_PER_SECOND := 0.2
 
 var dead := false
 var in_fire := false
+var is_running := false
+var stamina_depleted := false
 
 var health := 1.0
+var stamina := 1.0
 var coldness := 0.0
 var hunger := 0.0
 
 func _ready():
 	get_tree().call_group("health_subscriber", "_on_health_changed", health)
+	get_tree().call_group("stamina_subscriber", "_on_stamina_changed", stamina)
 	get_tree().call_group("hunger_subscriber", "_on_hunger_changed", hunger)
 	get_tree().call_group("coldness_subscriber", "_on_coldness_changed", coldness)
 
@@ -24,6 +30,7 @@ func _process(delta: float) -> void:
 	if dead:
 		return
 	update_health(delta)
+	update_stamina(delta)
 	update_hunger(delta)
 	update_coldness(delta)
 
@@ -46,6 +53,16 @@ func update_health(delta: float) -> void:
 		get_tree().call_group("health_subscriber", "_on_health_changed", health)
 		if health <= 0.0:
 			die()
+
+func update_stamina(delta: float) -> void:
+	if is_running:
+		stamina = max(stamina - STAMINA_DECAY_PER_SECOND * delta, 0.0)
+	else:
+		stamina += STAMINA_REPLENISH_PER_SECOND * delta
+		if stamina >= 1.0:
+			stamina = 1.0
+			stamina_depleted = false
+	get_tree().call_group("stamina_subscriber", "_on_stamina_changed", stamina)
 
 func die():
 	dead = true
@@ -70,7 +87,19 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	).normalized()
 
-	var speed = RUNNING_SPEED if Input.is_action_pressed("run") else WALKING_SPEED
+	var wants_to_run := Input.is_action_pressed("run")
+	var speed: float
+	if wants_to_run:
+		if !stamina_depleted && stamina > 0.0:
+			speed = RUNNING_SPEED
+			is_running = true
+		else:
+			stamina_depleted = true
+			speed = WALKING_SPEED
+			is_running = false
+	else:
+		speed = WALKING_SPEED
+		is_running = false
 
 	move_and_slide(direction * speed)
 	if direction != Vector2.ZERO:
